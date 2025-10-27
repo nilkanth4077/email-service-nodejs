0c732814061a43a94ab81537bef423849cf02011
nilkanth4077 <nilkanth4077@gmail.com>
2025-10-27 10:31:11 +0530

    Consolling request body

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
 
-// Email send endpoint
 app.post("/send-email", async (req, res) => {
     const { to, subject, text, html } = req.body;
+    console.log("Incoming body: ", req.body);
 
     if (!to || !subject || (!text && !html)) {
         return res.status(400).json({ message: "Missing required fields" });
